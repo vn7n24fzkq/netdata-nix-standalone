49f44a238adda81afd092d73e55225d674132ec6
Casper <vn7n24fzkq@gmail.com>
2026-03-24 13:51:03 +0800

    fix: fix netdata build and logs

--- flake.nix
+++ flake.nix
@@ -14,11 +14,39 @@
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+
+          # Pre-fetch the dashboard since Nix sandbox has no network access
+          dashboard = pkgs.fetchurl {
+            url = "https://app.netdata.cloud/agent.tar.gz";
+            hash = "sha256-CSu9s0iR6Ja7sMaabKum/7QU6NU1qYWhzhmTEZVVsyY=";
+          };
+
           netdata-patched = pkgs.netdata.overrideAttrs (oldAttrs: {
             patches = (oldAttrs.patches or []) ++ [
               ./patches/bypass-login.patch
             ];
+
+            # nixpkgs disables dashboard by default, we need to enable it
+            cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+              "-DENABLE_DASHBOARD:BOOL=TRUE"
+              "-DDASHBOARD_URL=file://${dashboard}"
+            ];
           });
+
+          entrypoint = pkgs.writeShellScript "netdata-entrypoint" ''
+            # Create all required directories
+            mkdir -p /var/log/netdata
+            mkdir -p /var/lib/netdata
+            mkdir -p /var/cache/netdata
+            mkdir -p /var/run
+            mkdir -p /etc/netdata
+            mkdir -p /tmp
+            chmod 1777 /tmp
+
+            # -D: Don't fork (foreground)
+            # -s /host: Use /host as system prefix for monitoring host from container
+            exec ${netdata-patched}/bin/netdata -D -s /host "$@"
+          '';
         in
         {
           netdata = netdata-patched;
@@ -32,16 +60,40 @@
               netdata-patched
               bashInteractive
               coreutils
+              procps
+              gawk
+              gnugrep
+              findutils
+              docker-client   # For Docker container monitoring
+              dockerTools.fakeNss
             ];
 
+            extraCommands = ''
+              mkdir -p var/log/netdata
+              mkdir -p var/lib/netdata
+              mkdir -p var/cache/netdata
+              mkdir -p var/run
+              mkdir -p etc/netdata
+              mkdir -p tmp
+              chmod 1777 tmp
+            '';
+
             config = {
-              Cmd = [ "${netdata-patched}/bin/netdata" "-D" ];
+              Cmd = [ "${entrypoint}" ];
               ExposedPorts = {
                 "19999/tcp" = {};
               };
               Env = [
                 "NETDATA_LISTENER_PORT=19999"
+                "NETDATA_STOCK_CONFIG_DIR=${netdata-patched}/share/netdata/conf.d"
+                "NETDATA_WEB_DIR=${netdata-patched}/share/netdata/web"
+                "NETDATA_CACHE_DIR=/var/cache/netdata"
+                "NETDATA_LIB_DIR=/var/lib/netdata"
+                "NETDATA_LOG_DIR=/var/log/netdata"
+                "NETDATA_LOCK_DIR=/var/lib/netdata/lock"
+                "NETDATA_USER_CONFIG_DIR=/etc/netdata"
               ];
+              WorkingDir = "/";
             };
           };
         }
